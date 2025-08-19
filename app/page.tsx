22a6751f5c230f9f06e62f24c4714a83ca834ae8
william <mbonguefeukou@gmail.com>
2025-08-19 18:51:28 +0200

    les accents

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="text-4xl text-gray">Je suis pas là hein</h1>
-      <p className="text-lg text-gray-600">Cest pas moi qui ai écrit ça</p>
+      <h1 className="text-4xl text-gray">Je suis pas la hein</h1>
+      <p className="text-lg text-gray-600">Cest pas moi qui ai ecrit ça</p>
       <div className="border w-1/5 h-1/5 bg-gray-600"></div>
     </div>
   );
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -5,7 +5,7 @@ import Register from '@/ui/register'
 export default function RegisterPage() {
     return (
         <div className="container mx-auto flex flex-col items-center p-4">
-            <h1 className="text-3xl font-bold mb-4">S'inscrire</h1>
+            <h1 className="text-3xl font-bold mb-4">{"S'inscrire"}</h1>
             <Register />
         </div>
     )
