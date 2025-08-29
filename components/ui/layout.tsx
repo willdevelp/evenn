715274553965707b85840770e2d2fbace547125e
william <mbonguefeukou@gmail.com>
2025-08-29 03:14:35 +0200

    debut de projet

--- components/ui/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { PropsWithChildren } from "react";
-
-export const PageLayout = (props: PropsWithChildren) => {
-    return (
-        <div className="flex flex-col w-full mx-auto p-4">
-            {props.children}
-        </div>
-    )
-}
